318bff2e90e2dbfa017601f189b54fece533b851
CoreyKatzburg <coreykny614@gmail.com>
2023-01-29 02:28:23 -0500

    Initial version

---
+++ src/main/java/com/shadow614/mtapack/MtaPackClient.java
@@ -0,0 +1,10 @@
+package com.shadow614.mtapack;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MtaPackClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
