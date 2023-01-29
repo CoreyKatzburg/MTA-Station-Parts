92338f958a3086e38618cf7767823087f360060a
CoreyKatzburg <coreykny614@gmail.com>
2023-01-29 00:20:53 -0500

    one

---
+++ src/main/java/com/shadow614/mtapack/MtaPack.java
@@ -0,0 +1,15 @@
+package com.shadow614.mtapack;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MtaPack implements ModInitializer {
+	public static final String MOD_ID = "mtapack";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
